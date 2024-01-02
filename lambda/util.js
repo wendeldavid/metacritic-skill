c71188b3040fafd50b9523d8b3b99f22f795a065
wendeldavid <wendel.przygoda@rdstation.com>
2024-01-02 20:23:55 -0300

    test interpolate result

--- lambda/util.js
+++ lambda/util.js
@@ -1,26 +1,10 @@
-const AWS = require('aws-sdk');
-
-const s3SigV4Client = new AWS.S3({
-    signatureVersion: 'v4',
-    region: process.env.S3_PERSISTENCE_REGION
-});
-
-module.exports.getS3PreSignedUrl = function getS3PreSignedUrl(s3ObjectKey) {
-
-    const bucketName = process.env.S3_PERSISTENCE_BUCKET;
-    const s3PreSignedUrl = s3SigV4Client.getSignedUrl('getObject', {
-        Bucket: bucketName,
-        Key: s3ObjectKey,
-        Expires: 60*1 // the Expires is capped for 1 minute
-    });
-    console.log(`Util.s3PreSignedUrl: ${s3ObjectKey} URL ${s3PreSignedUrl}`);
-    return s3PreSignedUrl;
-
-}
-
-module.exports.interpolateString = function interpolateString(template, valueMap) {
+const interpolateString = function(template, valueMap) {
     return Object.keys(valueMap).reduce((result, key) => {
       const regex = new RegExp(`\\$\\{${key}\\}`, 'g');
       return result.replace(regex, valueMap[key]);
     }, template);
-  }
+}
+
+module.exports  = {
+    interpolateString : interpolateString
+}
