8a263b3aef3dc0b9e15b11c227319b019575d2ea
wendeldavid <wendel.przygoda@rdstation.com>
2024-01-02 20:20:00 -0300

    test interpolate result

--- lambda/index.js
+++ lambda/index.js
@@ -6,7 +6,7 @@
 const Alexa = require('ask-sdk-core');
 const i18n = require("i18next");
 const languageStrings = require("./languageStrings");
-require(".utils.js");
+require("./utils.js");
 const sprintf = require("i18next-sprintf-postprocessor");
 
 const metacritic = require("./metacritic.js");
