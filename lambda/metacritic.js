09a76b06e35166af215940a488a2c71a4b23d7d7
wendeldavid <wendel.przygoda@rdstation.com>
2024-01-04 20:40:10 -0300

    fix gamesearch

--- lambda/metacritic.js
+++ lambda/metacritic.js
@@ -4,10 +4,9 @@ const API_KEY = process.env.RAWG_API_KEY || "e30cbb24af9548ab861b932c43604540";
 
 const getGame = async function(name) {
     const gameName = encodeURIComponent(name);
-    // const gameName = name;
     let config = {
         method: 'get',
-        url: `https://api.rawg.io/api/games?key=${API_KEY}&ordering=rating&search_exact=true&search=${gameName}&page_size=1`,
+        url: `https://api.rawg.io/api/games?key=${API_KEY}&ordering=rating&search_exact=true&page_size=1&search=${gameName}`,
         headers: { }
     };
 
